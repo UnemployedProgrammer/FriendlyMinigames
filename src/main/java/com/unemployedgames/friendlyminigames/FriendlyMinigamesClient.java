915d8ec06a430826513170de1f9e63478fe56ee1
BuildTools <unconfigured@null.spigotmc.org>
2023-12-09 14:04:59 +0100

    Initial commit

---
+++ src/main/java/com/unemployedgames/friendlyminigames/FriendlyMinigamesClient.java
@@ -0,0 +1,10 @@
+package com.unemployedgames.friendlyminigames;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class FriendlyMinigamesClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ src/main/java/com/unemployedgames/friendlyminigames/FriendlyMinigamesDataGenerator.java
@@ -0,0 +1,11 @@
+package com.unemployedgames.friendlyminigames;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class FriendlyMinigamesDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
